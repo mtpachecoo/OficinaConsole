b18cf6be79276b036d2becf9c5bd20cb454c3196
Pacheco <Pacheco@NASSIANIA>
2026-03-20 17:46:22 -0300

    feat: Add entidade Veiculo

---
+++ OficinaConsole/Entities/Veiculo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OficinaConsole.Entities
+{
+    public abstract class Veiculo
+    {
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int Placa { get; private set; }
+
+        public Veiculo() { }
+
+        public Veiculo(string modelo, string marca, int placa)
+        {
+            Modelo = modelo;
+            Marca = marca;
+            Placa = placa;
+        }
+    }
+}
+
+/*
+Veiculo (classe base abstrata)
+├── Carro
+└── Moto
+
+Mecanico (classe concreta)
+
+Servico (classe concreta)
+
+OrdemDeServico (classe concreta)
+└── usa: Veiculo, Mecanico, List<Servico>, IPagamento
+
+IPagamento (interface)
+├── PagamentoPix
+├── PagamentoCartao
+└── PagamentoDinheiro
+*/
